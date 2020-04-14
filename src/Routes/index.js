8ca2abb707b4f2b55f1c900c93ad9d44400f16b2
parsercoder <parsercoder@gmail.com>
2020-04-14 13:49:46 +0200

    run decorators on routes

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,20 +1,20 @@
 import AuthRouters from './auth.routes'
-// import OrgRouters from './org.routes'
-// import PostRouters from './post.routes'
-// import MeetingRouters from './meeting.routes'
-// import VolunteerRouters from './volunteer.routes'
-// import SupporterRouters from './support.routes'
+import OrgRouters from './org.routes'
+import PostRouters from './post.routes'
+import MeetingRouters from './meeting.routes'
+import VolunteerRouters from './volunteer.routes'
+import SupporterRouters from './support.routes'
 import { Router } from 'express'
 
 const router = Router()
 
 // auth is public route that's why we separate it
 router.use('/auth', AuthRouters)
-// router.use('/api/organizations', OrgRouters)
-// router.use('/api/posts', PostRouters)
-// router.use('/api/meetings', MeetingRouters)
-// router.use('/api/volunteers', VolunteerRouters)
-// router.use('/api/supporters', SupporterRouters)
+router.use('/api/organizations', OrgRouters)
+router.use('/api/posts', PostRouters)
+router.use('/api/meetings', MeetingRouters)
+router.use('/api/volunteers', VolunteerRouters)
+router.use('/api/supporters', SupporterRouters)
 
 // export all of our routes for our entry file
 export default router
