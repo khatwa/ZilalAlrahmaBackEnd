4929255d467b793525ae1cfd7d96214b94df15e3
parsercoder <parsercoder@gmail.com>
2020-04-14 13:48:51 +0200

    delete request identifier proxy

--- src/Proxies/index.js
+++ src/Proxies/index.js
@@ -1,12 +1,9 @@
 // import protection proxy to be ran if api has been requested
 import ProtectionProxy from "./protection.proxy"
 // import request identifier to detect requested route
-import RequestIdentifier from "./request-identifier"
 // import security functions
 import { verifyToken } from "../security"
 
 // inject verifyToken into the protection proxy to validate user credentials
 export const protectionProxy = ProtectionProxy(verifyToken)
 
-// export request identifier function to express adapter
-// export const requestIdentifier = RequestIdentifier(Authorization)
